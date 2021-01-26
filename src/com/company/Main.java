9a6139670cd8134726251c69f7570beb40d806bb
alexsonsilva <21643@student.dorset-college.ie>
2021-01-26 15:31:52 +0000

    second commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,38 +1,49 @@
 package com.company;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import javax.swing.*;
+import java.sql.*;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        final String DB_url = "jdbc:mysql://localhost:3306/titanicmanifest";
+        final String DB_USER = "AlexsonSilva";
+        final String DB_PASSWORD = ".,Familia*25";
+
         try {
             Class.forName("com.mysql.jdbc.Driver");
 
             Connection con = DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/titanicmanifest", "AlexsonSilva", ".,Familia*25");
+                    DB_url, DB_USER, DB_PASSWORD);
 
             Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery("select * from titanic");
+            String sqlQuery = "select * from titanic";
+            ResultSet rs = stmt.executeQuery(sqlQuery);
 
-            System.out.println("before while");
+            ResultSetMetaData metaData = rs.getMetaData();
+            int numberOfColumns = metaData.getColumnCount();
+            for (int i = 1; i <= numberOfColumns; i++){
+                System.out.printf("%-8s\t", metaData.getColumnName(i));
+            }
+            System.out.println();
+
+//            System.out.println("before while");
 
             while (rs.next()) {
-                System.out.println(rs.getString(4) + " " + rs.getString(5) + " " +
-                        rs.getString(6));
+//                System.out.println(rs.getString(4) + " " + rs.getString(5) + " " +
+//                        rs.getString(6));
+                System.out.println(rs.getInt(1) + "\t  " + rs.getInt(2) + " \t " + rs.getInt(3) + " \t " + rs.getString(4));
             }
-            System.out.println("after while");
+//            System.out.println("after while");
 
-            con.close();
+//            con.close();
 
         } catch (Exception e) {
             System.out.println(e.toString());
 
         } finally {
-            System.out.println("finally");
+//            System.out.println("finally");
 
         }
 
